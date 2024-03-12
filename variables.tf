4a232f4936458c18e7b8a2d4494c151f4233d5bf
nako <you@example.com>
2024-03-12 10:48:48 +0500

    key files

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "key_pair_permission" {
+  description = "Permission to set for the private key file."  
+  type        = list(number)
+}
+
+variable "private_key_path" {
+  description = "Path to the private key file."  
+   type        = list(string)
+}
+
+variable "public_key_path" {
+  description = "Path to the public key file."  
+   type        = list(string)
+}
+
+variable "key_name" {
+  description = "Name of the key pair."  
+   type        = list(string)
+}
+
+variable "private_key_algorithm" {
+  description = "Algorithm used for generating the private key."  
+   type        = list(string)
+}
+
+variable "private_key_rsa_bits" {
+  description = "the size of the generated RSA key, in bits"  
+  type        = list(number)
+}
